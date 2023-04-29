a6afbf634aeb6a9558fec9dcf0de6fd426dd5f14
Vivek Shah <94179705+Vivek-C-Shah@users.noreply.github.com>
2023-04-30 01:00:02 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Header = () => {
 	return(
 		      <footer className="footer">
             <div className="d-sm-flex justify-content-center">
-              <span className="float-none text-center d-block mt-1 mt-sm-0 text-center space-x-2">Created by <a href={"https://www.linkedin.com/in/vivek-shah-b65507231/"} target="_blank" rel="noopener noreferrer" className="round-link">
+              <span className="float-none text-center d-block mt-1 mt-sm-0 text-center space-x-2">Created by <a href={"https://www.linkedin.com/in/vivek-shah-b65507231/"} target="_blank" rel="noopener noreferrer">
 								Vivek
 								</a> and <a> </a>
                 <a href={"https://www.linkedin.com/in/vrajshah113"} target="_blank" rel="noopener noreferrer">

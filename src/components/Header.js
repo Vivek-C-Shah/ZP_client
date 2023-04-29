81e9a5e8c6cc39d990a2c8bc66671b9d73b1a7c7
Vivek Shah <94179705+Vivek-C-Shah@users.noreply.github.com>
2023-04-30 00:48:35 +0530

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+	return(
+		      <footer className="footer">
+            <div className="d-sm-flex justify-content-center">
+              <span className="float-none text-center d-block mt-1 mt-sm-0 text-center space-x-2">Created by <a href={"https://www.linkedin.com/in/vivek-shah-b65507231/"} target="_blank" rel="noopener noreferrer" className="round-link">
+								Vivek
+								</a> and <a> </a>
+                <a href={"https://www.linkedin.com/in/vrajshah113"} target="_blank" rel="noopener noreferrer">
+								 Vraj
+								</a>.Copyright ©  2023. All rights reserved.</span>
+            </div>
+          </footer>
+		)
+
+}
+
+export default Header;
